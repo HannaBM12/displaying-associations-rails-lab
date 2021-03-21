299cdf6c7a4af45f69d2bf04de0605e6c1fd53c3
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-20 23:36:27 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.destroy_all
+Song.destroy_all
+
+a1 = Artist.create(name: "Hella Alemayehu")
+a2 = Artist.create(name: "Alex Bueman")
+a3 = Artist.create(name: "Meledi Ekram")
+a4 = Artist.create(name: "Brayen Micheal")
+
+
+s1 = Song.create(title: "My heart", artist_id: a1.id)
+s2 = Song.create(title: "Excellent", artist_id: a1.id)
+s3 = Song.create(title: "Rainbow", artist_id: a2.id)
+s4 = Song.create(title: "Sunshine", artist_id: a3.id)
+s5 = Song.create(title: "Magnificient", artist_id: a3.id)
+s6 = Song.create(title: "Broken", artist_id: a3.id)
+s7 = Song.create(title: "Sublime", artist_id: a4.id)
+s8 = Song.create(title: "Marie", artist_id: a4.id)
